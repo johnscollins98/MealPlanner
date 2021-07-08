1ff5056f6b29276f8edde017a17172369f994426
John Collins <johnscollins98@gmail.com>
2021-07-08 20:45:53 +0100

    add filtering to recipe page

--- MealPlanner/Pages/Index.cshtml.cs
+++ MealPlanner/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 using MealPlanner.Core;
 using MealPlanner.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
 namespace MealPlanner.Pages.Recipes
@@ -8,17 +10,39 @@ namespace MealPlanner.Pages.Recipes
     public class IndexModel : PageModel
     {
         private readonly IRecipeRepository recipeData;
+        private readonly IHtmlHelper htmlHelper;
 
         public IEnumerable<Recipe> Recipes { get; set; }
 
-        public IndexModel(IRecipeRepository recipeData)
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        public IEnumerable<SelectListItem> Times { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public MealCategory? CategoryFilter { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public MealTime? TimeFilter { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string NameFilter { get; set; }
+
+        public IndexModel(IRecipeRepository recipeData, IHtmlHelper htmlHelper)
         {
             this.recipeData = recipeData;
+            this.htmlHelper = htmlHelper;
         }
 
         public void OnGet()
         {
-            Recipes = recipeData.All();
+            Categories = htmlHelper.GetEnumSelectList<MealCategory>();
+            Times = htmlHelper.GetEnumSelectList<MealTime>();
+
+            Recipes = recipeData.Find(r =>
+                (CategoryFilter == null || r.Category == CategoryFilter)
+                && (TimeFilter == null || r.Time == TimeFilter)
+                && (string.IsNullOrEmpty(NameFilter) || r.Name.ToLower().Contains(NameFilter.ToLower()))
+            );
         }
     }
 }
